c75b57e837329ee9aa6c86ed2848bf18d05f4e15
mark bashir-ugwi <beejayphil@gmail.com>
2023-04-24 10:51:49 +0100

    added URL store

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const urlStore = {};
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
