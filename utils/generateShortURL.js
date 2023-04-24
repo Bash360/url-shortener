20e5cf5d2b6e4e1d226c3ec4997f1488e02cddcc
mark bashir-ugwi <beejayphil@gmail.com>
2023-04-24 03:28:24 +0100

    added function to genrate random characters

---
+++ utils/generateShortURL.js
@@ -0,0 +1,11 @@
+function generateShortURL(length=6) {
+   let result = '';
+  const base62Chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * base62Chars.length);
+    result += base62Chars[randomIndex];
+  }
+  return result;
+}
+
+module.exports = generateShortURL;
