9e41a45995a20021f0e675ffd7526566ffdf6ac4
mark bashir-ugwi <beejayphil@gmail.com>
2023-04-24 19:16:21 +0100

    app should only listen when its not test

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const urlRoutes = require('./routes/url.routes');
 const app = express();
+require('dotenv').config();
 
 app.disable('x-powered-by');
   app.use(express.json({ limit: "5mb" }));
 app.use('/', [urlRoutes]);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+if (process.env.NODE_ENV === "development") {
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}
 
 module.exports = app;
